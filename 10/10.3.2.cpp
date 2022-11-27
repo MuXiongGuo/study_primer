4951f1b4d006a3c0a7dda2b70fd126cb70a84f9d
muxiongguo <1721030720@qq.com>
2022-11-28 00:25:30 +0800

    🐨 10.3.2

--- 10/10.3.2.cpp
+++ 10/10.3.2.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+// biggies
+void Biggies(vector<string> &words, vector<string>::size_type sz) {
+    elimDups(words);
+    stable_sort(words.begin(), words.end(), isShorter);
+    auto wc = find_if(words.begin(), words.end(), [sz](const string &a) { return a.size() >= sz; });
+    auto count = words.end() - wc;
+    cout << count << " " << make_plural(count, "word", "s") << " of length " << sz << " or longer" << endl;
+    for_each(wc, words.end(), [](const string &s) { cout << s << " "; });
+    cout << endl;
+}
+
 int main() {
     // 10.14
     auto f1 = [](int a, int b) -> int { return a + b; };
